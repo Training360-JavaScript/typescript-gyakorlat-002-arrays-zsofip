ce626d33b1d8aeb09eda6eb1f8f9efa8d2741220
zsofip <91950899+zsofip@users.noreply.github.com>
2022-01-06 22:26:04 +0100

    done

--- src/lesson-01.ts
+++ src/lesson-01.ts
@@ -5,7 +5,7 @@ export default class calculator {
     // Hozd létre a következő változókat a megfelelő típusannotációval.
     
     /** @type {Array} points - számok tömbje, 1-től 10-ig egyesével. */
-
+    points: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy felhasználót reprezentál.
@@ -13,7 +13,16 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} users - minden elem egy felhasználó.
      */
-
+    users: Array<{name: string, email: string}> = [
+        {
+            name: 'Piros Alma',
+            email: 'prslm@gmail.com'
+        },
+        {
+            name: 'Fekete Farkas',
+            email: 'ff@gmail.com'
+        }
+    ];
     
     /** 
      * A tömb minden eleme legyen egy objektum, amely egy terméket reprezentál.
@@ -22,12 +31,24 @@ export default class calculator {
      * Inicializáld a tömböt, legyen legalább két eleme.
      * @type {Array} products - minden elem egy termék.
      */    
-
+     products: Array<{name: string, price: number, stock: number, active?: boolean}> = [
+        {
+            name: 'kalap',
+            price: 7500,
+            stock: 8
+        },
+        {
+            name: 'sapka',
+            price: 4500,
+            stock: 26,
+            active: true
+        }
+    ];
     
     /** 
      * Inicializálj egy tuple típust, azaz adj neki értéket is.
      * @type {Tuple} role - number, string, boolean. 
      */
-
+    role: [number, string, boolean] = [76, 'hal', false];
 
 }
